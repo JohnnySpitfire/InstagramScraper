c7c42679ee48acdf63bdb8f834ef739a11c4f12d
JohnnySpitfire <benanthonyjohn@gmail.com>
2022-11-28 14:38:22 +1300

    finished?

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests as req
 import threading
 import sys
 import itertools
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -147,19 +148,20 @@ def clear_notification_popups(driver):
     not_now_btn = driver.find_element(By.XPATH, "//button[contains(text(), \"Not Now\")]")
     not_now_btn.click()
     wait_for_response(driver, "XPATH_VALUE", "Masters of Spinjitzu", 5)
-    # message_box = driver.find_element(By.XPATH, "//div[@class=\"_ab5z _ab5_\"]")
-    # message_box.click()
     
 def load_messages(driver, actions):
     global LOADING_MESSAGES, LOADING_ANIMATION_ACTIVE
     LOADING_MESSAGES, LOADING_ANIMATION_ACTIVE = True, True
     print("Loading Messages....")
+    wait_count = 1
     while LOADING_MESSAGES is True:
         wait_for_response(driver, "XPATH_CLASS", "_aacl _aacn _aacu _aacy _aada")
         try:
             message_box = driver.find_element(By.XPATH, "//div[@class=\"_aacl _aacn _aacu _aacy _aada\"]")
             message_box.click()
         except:
+            print(f"{wait_count} Waiting!")
+            wait_count += 1
             time.sleep(60)
             message_box = driver.find_element(By.XPATH, "//div[@class=\"_aacl _aacn _aacu _aacy _aada\"]")
             message_box.click()
@@ -176,12 +178,7 @@ def get_messages(driver):
     print("Getting Messages....")
     box = driver.find_element(By.XPATH, "//div[@class=\"_ab5z _ab5_\"]/div")
     messages_and_usernames = box.find_elements(By.XPATH, "./div[@class=\"_ab8w  _ab94 _ab96 _ab9f _ab9k _ab9p _abcm\"] | ./div[@class=\"_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9p _abcm\"]")
-    message_dict = {}
-    line_count = len(messages_and_usernames)
-    for index, line in enumerate(messages_and_usernames):
-        if "AM" in line.text or "PM" in line.text:
-            messages_and_usernames.remove(line)
-        print_progress_bar(index, line_count, "Removing Redundant Elements:", f"{line_count - len(messages_and_usernames)} lines removed")
+    message_user = {}
     i = len(messages_and_usernames) - 1
     while i >= 0:
         name_found = False
@@ -192,80 +189,141 @@ def get_messages(driver):
             while name_found == False and j >= 0:
                 try:
                     if messages_and_usernames[j].get_dom_attribute("class") == "_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9p _abcm":
-                        name_found = True
-                        username = messages_and_usernames[j].text
-                        if username == '':
-                            username = "ben_john555"
-                        if username not in message_dict:
-                            message_dict[username] = [messages_and_usernames[i]]
-                        else:
-                            message_dict[username].append(messages_and_usernames[i])
+                        try:
+                            try:
+                                messages_and_usernames[j].find_element(By.XPATH, "./div")
+                            except Ex.NoSuchElementException:
+                                username = "ben_john555"
+                            else:                        
+                                username = messages_and_usernames[j].find_element(By.XPATH, "./div/div[@class=\"_aacl _aacn _aacu _aacy _aada\"]").text
+                            name_found = True
+                            if username not in message_user:
+                                message_user[username] = [messages_and_usernames[i]]
+                            else:
+                                message_user[username].append(messages_and_usernames[i])
+                        except:
+                            pass
                 except:
                     print(Ex.StaleElementReferenceException)
+                    get_messages(driver)
+                    return
                 else:
                     j -= 1
         i -= 1
     print("Messages Found!")
+    
     audio_messages = []
     image_messages = []
+    video_messages = []
     text_messages = []
-    for user in message_dict:
+    
+    for user in message_user:
         i = 0
-        while i < len(message_dict[user]):
-            print_progress_bar(i, len(message_dict[user]), f"Categorizing {user}'s Messages", "Completed")
+        while i < len(message_user[user]):
+            print_progress_bar(i, len(message_user[user]), f"Categorizing {user}'s Messages", "Completed")
             try:
-                message_dict[user][i].find_element(By.TAG_NAME, "audio")
+                message_user[user][i].find_element(By.TAG_NAME, "audio")
             except Ex.NoSuchElementException:
                 try:
-                    message_dict[user][i].find_element(By.XPATH, "//div[@class=\"_aacl _aaco _aacu _aacx _aad6 _aade\"]")
+                    message_user[user][i].find_element(By.TAG_NAME, "video")
                 except Ex.NoSuchElementException:
                     try:
-                        message_dict[user][i].find_element(By.XPATH, "//img[not(@class)]")
+                        message_user[user][i].find_element(By.XPATH, ".//img[not(@class)]")
                     except Ex.NoSuchElementException:
-                        raise Exception("Unexpected Element Type")
+                        try:
+                            message_user[user][i].find_element(By.XPATH, "//div[@class=\"_aacl _aaco _aacu _aacx _aad6 _aade\"]")
+                        except Ex.NoSuchElementException:
+                            raise Exception("Unexpected Element Type")
+                        else:
+                            text_messages.append((user, message_user[user][i]))
+                            i += 1
                     else:
-                        image_messages.append((user, message_dict[user][i]))
+                        image_messages.append((user, message_user[user][i]))
                         i += 1
                 else:
-                    text_messages.append((user, message_dict[user][i]))
+                    video_messages.append((user, message_user[user][i]))
                     i += 1
             else:
-                audio_messages.append((user, message_dict[user][i]))
+                audio_messages.append((user, message_user[user][i]))
                 i += 1
-                
+        print("\n")
+    
     download_voice_messages(audio_messages)
     download_images(image_messages)
-    process_text_messages(text_messages, audio_messages)
-    
-    print_user_stats(message_dict, audio_messages)
+    download_videos(video_messages)
+    process_text_messages(text_messages)
+    print_user_stats(message_user, audio_messages, image_messages, video_messages)
 
-def print_user_stats(message_dict, audio_messages):
+def print_user_stats(message_dict, audio_messages, image_messages, video_messages):
+    
+    total_message_count = 0
+    total_audio_message_count = 0
+    total_image_message_count = 0
+    total_video_message_count = 0
+    
     for user in message_dict:
         audio_message_count = 0
+        image_message_count = 0
+        video_message_count = 0
+        
         for username, message in audio_messages:
             if username == user:
                 audio_message_count += 1
-        print(f"{user}'s Total Number of Messages: {message_dict[user].count()}")
+        for username, message in image_messages:
+            if username == user:
+                image_message_count += 1
+        for username, message in video_messages:
+            if username == user:
+                video_message_count += 1
+        
+        total_message_count += len(message_dict[user])
+        total_audio_message_count += audio_message_count
+        total_image_message_count += image_message_count
+        total_video_message_count += video_message_count
+        
+        print(f"{user}'s Total Number of Messages: {len(message_dict[user])}")
         print(f"{user}'s Total Number of Audio Messages: {audio_message_count}")
+        print(f"{user}'s Total Number of Image Messages: {image_message_count}")
+        print(f"{user}'s Total Number of Video Messages: {video_message_count}\n")
+
+    print(f"Total Number of Messages: {total_message_count}")
+    print(f"Total Number of Audio Messages: {total_audio_message_count}")
+    print(f"Total Number of Image Messages: {total_video_message_count}")
+    print(f"Total Number of Video Messages: {total_video_message_count}\n")
     
 def process_text_messages(text_messages):
     i = 0
-    with open("text_messages.csv", "xt") as f:        
+    with open("text_messages.csv", "xt", encoding='utf-8') as f:        
         for username, message in text_messages:
             print_progress_bar(i, len(text_messages), "Processing Text Messages:", "Processed")
-            f.write(f"{username},{message.text}")
+            f.write(f"{username},{ascii(message.text)},\n")
+            i += 1
+    print("\n")
 
+def download_videos(video_messages):
+    i = 0
+    for username, message in video_messages:
+        print_progress_bar(i, len(video_messages), "Downloading Videos:", "Downloaded")
+        i += 1
+        video_src = message.find_element(By.TAG_NAME, "source").get_dom_attribute("src")
+        video_file = req.get(video_src)
+        file_name = f"{i}_{username}"
+        with open(f"vid/{file_name}.mp4", "wb") as f:
+            f.write(video_file.content)
+    print("\n")
+    
 def download_images(image_messages):
     i = 0
     for username, message in image_messages:
         print_progress_bar(i, len(image_messages), "Downloading Images:", "Downloaded")
         i += 1
-        img_src = message.get_dom_attribute("src")
+        img_src = message.find_element(By.XPATH, ".//img[not(@class)]").get_dom_attribute("src")
         image_file = req.get(img_src)
         file_name = f"{i}_{username}"
         with open(f"img/{file_name}.jpg", "wb") as f:
             f.write(image_file.content)
-
+    print("\n")
+    
 def download_voice_messages(voice_messages):
     i = 0
     for username, message in voice_messages:
@@ -274,9 +332,10 @@ def download_voice_messages(voice_messages):
         audio_src = message.find_element(By.TAG_NAME, "source").get_dom_attribute("src")
         audio_file = req.get(audio_src)
         file_name = f"{i}_{username}"
-        with open(f"audio/{file_name}.mp3", "wb") as f:
+        with open(f"aud/{file_name}.mp3", "wb") as f:
             f.write(audio_file.content)
-
+    print("\n")
+    
 if __name__ == '__main__':
     driver = webdriver.Firefox()
     driver.maximize_window()
